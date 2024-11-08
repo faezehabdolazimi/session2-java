e2038d45582cd097bb69861e32b9c156b06d4770
faezehabdolazimi <75137625+faezehabdolazimi@users.noreply.github.com>
2024-11-08 22:35:57 +0330

    Update main.java

--- main.java
+++ main.java
@@ -8,5 +8,21 @@ public class main {
 
 	        student.notify("Welcome to the class!");  
 	        teacher.notify("Please check the assignments.");  
-	    }  
-	}
+
+        Circle circle = new Circle(5);  
+        Square square = new Square(4);  
+        Rectangle rectangle = new Rectangle(6, 3); 
+        System.out.println("Circle with radius 5:");  
+        System.out.printf("مساحت: %.2f\n", circle.area());  
+        System.out.printf("محیط: %.2f\n", circle.perimeter());  
+
+        System.out.println("\nA square with 4 sides:");  
+        System.out.printf("مساحت: %.2f\n", square.area());  
+        System.out.printf("محیط: %.2f\n", square.perimeter());  
+
+        System.out.println("\nA rectangle with a length of 6 and a width of 3:");  
+        System.out.printf("مساحت: %.2f\n", rectangle.area());  
+        System.out.printf("محیط: %.2f\n", rectangle.perimeter());  
+    }  
+}  
+	
